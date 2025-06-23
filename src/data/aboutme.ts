e0ba1540f87c13ec2baeb128bb3fde6a2fe1d23e
M. Murat Ardag <mmadatsci@gmail.com>
2025-06-23 15:49:28 +0200

    Update aboutme.ts

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -18,22 +18,22 @@ export interface AboutMe {
 }
 
 export const aboutMe: AboutMe = {
-  name: "Jane R. Smith",
-  title: "Ph.D. Candidate",
-  institution: "Stanford University",
+  name: "M. Murat Ardag",
+  title: "Ph.D.",
+  institution: " ",
   // Note that links work in the description
   description:
-    "I'm a final-year <a href='https://www.stanford.edu'>PhD candidate</a> working at the intersection of causal inference and machine learning. My research focuses on developing robust, interpretable systems that can reason about cause and effect in complex environments.",
-  email: "______@stanford.edu",
+    "This is the landing page of my personal websites. The following links provide information about my different roles.<br>My professional journey led me to the intersection of <a href=https://mmuratardag.github.io/data_science_website target=_blank>data science</a>, <a href=https://mmuratardag.github.io/cybersecurity_website target=_blank>cybersecurity</a>, and <a href=https://mmuratardag.github.io/academic_website target=_blank>academia</a>.",
+  email: "email@smmuratardag.net",
   imageUrl:
-    "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
-  githubUsername: "janesmith",
-  linkedinUsername: "janesmith",
-  twitterUsername: "janesmith",
-  blogUrl: "https://",
-  cvUrl: "https://",
-  institutionUrl: "https://www.stanford.edu",
+    "/img/mma_c_gp.jpg",
+  // googleScholarUrl: "https://scholar.google.com/citations?user=6MoKqzkAAAAJ&hl=en",
+  // githubUsername: "mmuratardag",
+  // linkedinUsername: "mmuratardag",
+  // twitterUsername: "mmuratardag",
+  // blogUrl: "",
+  // cvUrl: "",
+  // institutionUrl: "",
   // altName: "",
-  // secretDescription: "I like dogs.",
+  // secretDescription: "testing if I like this template as a landing page for multiple websites",
 };
